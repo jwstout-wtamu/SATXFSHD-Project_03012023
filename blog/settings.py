cbddc1d967347361972389271da969f0d45a6395
jwstout-wtamu <60401878+jwstout-wtamu@users.noreply.github.com>
2022-11-21 14:59:42 -0600

    removed AZ URL Update

--- blog/settings.py
+++ blog/settings.py
@@ -30,8 +30,8 @@ SECRET_KEY = environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [".local", "satxfshd.local", "satxfshd.herokuapp.com", ".herokuapp.com", "localhost", "127.0.0.1"]  # new
-#added Azure URL
+ALLOWED_HOSTS = ["satxfshd.local", "satxfshd.herokuapp.com", ".herokuapp.com", "localhost", "127.0.0.1"]  # new
+#removed Azure URL
 
 # Application definition
 
