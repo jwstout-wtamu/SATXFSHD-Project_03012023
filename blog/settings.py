29d9a8c457cafeda75f9709133197bf8fb4a8428
jwstout-wtamu <60401878+jwstout-wtamu@users.noreply.github.com>
2022-11-21 13:26:19 -0600

    adding azure

--- blog/settings.py
+++ blog/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["satxfshd.herokuapp.com", ".herokuapp.com", "localhost", "127.0.0.1"]  # new
+ALLOWED_HOSTS = ["msdocs-python-webapp-quickstart-satxfshd.azurewebsites.net", "satxfshd.herokuapp.com", ".herokuapp.com", "localhost", "127.0.0.1"]  # new
 
 
 # Application definition
